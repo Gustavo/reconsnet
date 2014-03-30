718852026fbfd7288fc85f343e3f085e0acb1a69
Gustavo Rahal <grahal@vnet.linux.ibm.com>
2014-03-30 18:30:24 -0300

    Backup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Recons::Application.config.secret_key_base = '64faaea5308ff0ee46eae2af7b7a8e46b020a3f3242d034ba60790e468caace0138152a92dc1ef46559ad1178381cdf5cf739283b9b3f6d99db9ed1ada695377'
+ReconsNet::Application.config.secret_key_base = '64faaea5308ff0ee46eae2af7b7a8e46b020a3f3242d034ba60790e468caace0138152a92dc1ef46559ad1178381cdf5cf739283b9b3f6d99db9ed1ada695377'
